42e16e94fbbfd0ec48c8b73a9d8866de17035dc7
Suraj Kumar Didwania <sdidwani@jcp.com>
2022-05-05 19:35:25 -0500

    Saving Some Changes

--- venv/Server/request_handler.py
+++ venv/Server/request_handler.py
@@ -6,4 +6,22 @@ from .game import Game
 from queue import Queue
 import threading
 
-c
+def connection_thread():
+    server = ""
+    port = 5555
+
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+    try:
+        s.bind(server,port)
+    except socket.error as e:
+        str(e)
+
+    s.listen()
+    print ("Waiting for a connection, Server Started")
+
+    while True:
+        conn, addr = s.accept()
+        print ("[CONNECT] New Connection")
+
+        threading.Thread(target=, (addr,""))
